7365f0a7e3225f093bb8cbaeb83b8de9647a77a8
Tomwjdm <159501507+Tomwjdm@users.noreply.github.com>
2024-03-06 14:46:15 +1300

    Two dice funtion

--- C_04_Dice_simulator.py
+++ C_04_Dice_simulator.py
@@ -4,8 +4,7 @@ import random
 # generates an integer between 0 and 6
 # to simulate roll of a die
 def roll_die():
-    result = random.randint(1, 6)
-    return result
+    result = random.randint(1,6)
 
 
 
@@ -15,18 +14,18 @@ for item in range(1, 10):
     user_score = 0
     double_score = "no"
 
-    # Roll two dice
-    roll_1 = roll_die()
-    roll_2 = roll_die()
+# Roll two dice
+roll_1 = roll_die()
+roll_2 = roll_die()
 
-    # checks if we have a double score opportunity
-    if roll_1 == roll_2:
-        double_score = "yes"
+# checks if we have a double score opportunity
+if roll_1 == roll_2:
+    double_score = "yes"
 
 
-    # Find the total points (so far)
-    user_points = roll_1 + roll_2
+# Find the total points (so far)
+user_points = roll_1 + roll_2
 
-    # Show the user the result
-    print(f"die 1: {roll_1} \t Die 2: {roll_2} \t Points: {user_points}")
-    print(f"Double score opportunity: {double_score}")
+# Show the user the result
+print(f"die 1: {roll_1} \t Die 2: {roll_2} \t Points: {user_points}")
+print(f"Double score opportunity: {double_score}")
