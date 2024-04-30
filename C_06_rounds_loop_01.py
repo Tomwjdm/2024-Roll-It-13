2776de87356dac527993d5d5153de885b5aa82b0
Tomwjdm <159501507+Tomwjdm@users.noreply.github.com>
2024-04-30 12:57:29 +1200

    Basic Loop Rounds component

--- C_06_rounds_loop_01.py
+++ C_06_rounds_loop_01.py
@@ -1,16 +1,37 @@
+
+# Checks that users enter an integer
+# that is more than 13
 def int_check(question):
+
     while True:
-        error = "Please enter an integer that is 13 or more."
+
+        error = "Please enter an integer that is 13 or more. "
 
         try:
-            response = int(input(question))
+            response = int(input(question)
 
             # checks that the number is more than / equal to 13
             if response < 13:
-                print(error)
+                 print(error)
             else:
-                return response
+             return response
 
         except ValueError:
-            print(error)
+                print(error)
 
+# main routine goes here
+
+# initialise user score and computer score
+user_score = 0
+comp_score = 0
+
+target_score = int_check("Enter a target score")
+print(target_score)
+
+while user_score < target_score and comp_score < target_score:
+    print("Round headings goes here...")
+    add_points = int(input("How many points have been won?"))
+    user_score += add_points
+
+    print()
+    print(f"You final score is {user_score}")
